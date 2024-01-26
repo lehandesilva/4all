0c1f28664db5812d527bbd11545da2b50b7eefd9
NumeralArcher <lehande28@gmail.com>
2024-01-26 11:03:55 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Header from "./ui/header";
 import { fetchCourse } from "@/app/lib/data";
 
 export default async function Home() {
-  const courses = await fetchCourse();
-  console.log(courses[0].blocks[0].content);
   return (
     <main>
       <Header />
