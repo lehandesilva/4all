37749b24e12cce3a0df92c5a63bd5c9e5a863f4f
NumeralArcher <lehande28@gmail.com>
2024-01-30 02:54:51 +0000

    build: search component logic to redirect with query parameter

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,10 +1,12 @@
 import SideNav from "../ui/sidenav";
+import CoursesGrid from "../ui/courses-grid";
+import { courses } from "../lib/placeholder-data";
 
 export default function Search() {
   return (
     <main>
       <SideNav />
-      <p>Search page</p>
+      <CoursesGrid heading="Results" data={courses} />
     </main>
   );
 }
--- app/ui/searchBox.tsx
+++ app/ui/searchBox.tsx
@@ -1,32 +1,38 @@
 "use client";
+import React, { useState } from "react";
 import classes from "./searchBox.module.css";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 //import useDeboucedCallback from 'use-debounce';
 
 export default function SearchBox() {
+  const [searchQuery, setSearchQuery] = useState("");
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { replace } = useRouter();
+  const { replace, push } = useRouter();
 
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set("query", term);
+  const searchHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const params = new URLSearchParams();
+    if (searchQuery) {
+      params.set("query", searchQuery);
     } else {
       params.delete("query");
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}search?${params.toString()}`);
   };
 
   return (
     <div className={classes.searchBox}>
-      <input
-        className={classes.search}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("query")?.toString()}
-      />
+      <form onSubmit={searchHandler}>
+        <input
+          type="text"
+          className={classes.search}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+          defaultValue={searchParams.get("query")?.toString()}
+        />
+      </form>
     </div>
   );
 }
