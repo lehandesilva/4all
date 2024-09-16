8739953119ba9f9730419e39ec184f1dad01a545
blehhhhh :P <lehande28@gmail.com>
2024-09-16 06:02:23 +0100

    cock

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
-);
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(
+//   session({ secret: "my secret", resave: false, saveUninitialized: false })
+// );
+
 app.listen(5000);
