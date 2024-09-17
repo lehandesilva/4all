e4645a64437555c5c35f54f430bedf975b930b4b
blehhhhh :P <lehande28@gmail.com>
2024-09-17 01:30:23 +0100

    build: fixing file tree

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+# Routes
+
+users
+courses
---
+++ backend/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.listen(8080);
