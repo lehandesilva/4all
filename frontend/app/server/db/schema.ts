d2ee36a0d14b6f196e6b64462031dac17292800f
blehhhhh :P <lehande28@gmail.com>
2024-10-02 23:32:08 +0100

    build: added auth

--- frontend/app/server/db/schema.ts
+++ frontend/app/server/db/schema.ts
@@ -8,7 +8,6 @@ import {
   jsonb,
   boolean,
 } from "drizzle-orm/pg-core";
-import type { AdapterAccountType } from "next-auth/adapters";
 import { section_for_course, history, block } from "../definitions";
 
 export const roleEnum = pgEnum("role", ["admin", "user", "paidUser"]);
@@ -26,30 +25,6 @@ export const users = pgTable("user", {
   history: jsonb("history").$type<history[]>(),
   age: text("age").notNull(),
 });
-
-export const accounts = pgTable(
-  "account",
-  {
-    userId: text("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    type: text("type").$type<AdapterAccountType>().notNull(),
-    provider: text("provider").notNull(),
-    providerAccountId: text("providerAccountId").notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: text("token_type"),
-    scope: text("scope"),
-    id_token: text("id_token"),
-    session_state: text("session_state"),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
-    }),
-  })
-);
 
 export const categoriesTable = pgTable("categories", {
   id: text("id")
