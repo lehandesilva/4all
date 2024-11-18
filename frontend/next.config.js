77734e2bced6a994fcbc9bf3d803ceedb26da722
blehhhhh :P <lehande28@gmail.com>
2024-11-18 00:58:02 +0000

    fix: changed next.config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    AUTH_SECRET: process.env.AUTH_SECRET, // Expose AUTH_SECRET to the application
-  },
   images: {
     remotePatterns: [
       {
